dbdb4887440b383f2336ff75434542c6cbb25152
Ty Sanders <tysanders@tamu.edu>
2025-05-28 15:56:14 -0500

    migrated to postgres

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "drizzle-kit";
+import { type Config } from "drizzle-kit";
 
 import { env } from "~/env";
 
 export default {
-	schema: "./src/server/db/schema.ts",
-	dialect: "sqlite",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
-	tablesFilter: ["fintech_port_*"],
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["fintech_port_*"],
 } satisfies Config;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,6 @@
 import { relations, sql } from "drizzle-orm";
-import { index, primaryKey, sqliteTableCreator } from "drizzle-orm/sqlite-core";
-import type { AdapterAccount } from "next-auth/adapters";
-import { d } from "node_modules/drizzle-kit/index-BAUrj6Ib.mjs";
+import { index, pgTableCreator, primaryKey } from "drizzle-orm/pg-core";
+import { type AdapterAccount } from "next-auth/adapters";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -9,21 +8,21 @@ import { d } from "node_modules/drizzle-kit/index-BAUrj6Ib.mjs";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = sqliteTableCreator((name) => `fintech_port_${name}`);
+export const createTable = pgTableCreator((name) => `fintech_port_${name}`);
 
 // TRANSACTIONS
 export const transactions = createTable("transactions", (d) => ({
   id: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull()
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   userId: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull()
     .references(() => users.id),
   bankAccountId: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull(),
   amount: d.numeric()
 }));
@@ -31,12 +30,12 @@ export const transactions = createTable("transactions", (d) => ({
 // BANKACCOUNTS TODO: expand to add real accounts with stripe maybe
 export const bankAccounts = createTable("bankAccounts", (d) => ({
   id: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull()
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   userId: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull()
     .references(() => users.id),
 }))
@@ -44,16 +43,16 @@ export const bankAccounts = createTable("bankAccounts", (d) => ({
 // ROUNDUPS
 export const roundUps = createTable('roundUps', (d) => ({
   id: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull()
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   userId: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull()
     .references(() => users.id),
   transactionId: d
-    .text({ length: 255 })
+    .varchar({ length: 255 })
     .notNull()
     .references(() => transactions.id),
   amount_cents: d.integer(),
@@ -61,79 +60,77 @@ export const roundUps = createTable('roundUps', (d) => ({
 
 // USERS
 export const users = createTable("user", (d) => ({
-	id: d
-		.text({ length: 255 })
-		.notNull()
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	name: d.text({ length: 255 }),
-	email: d.text({ length: 255 }).notNull(),
-	emailVerified: d.integer({ mode: "timestamp" }).default(sql`(unixepoch())`),
-	image: d.text({ length: 255 }),
+  id: d
+    .varchar({ length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: d.varchar({ length: 255 }),
+  email: d.varchar({ length: 255 }).notNull(),
+  emailVerified: d
+    .timestamp({
+      mode: "date",
+      withTimezone: true,
+    })
+    .default(sql`CURRENT_TIMESTAMP`),
+  image: d.varchar({ length: 255 }),
 }));
 
-// ?
 export const usersRelations = relations(users, ({ many }) => ({
-	accounts: many(accounts),
+  accounts: many(accounts),
 }));
 
-// ACCOUNTS
 export const accounts = createTable(
-	"account",
-	(d) => ({
-		userId: d
-			.text({ length: 255 })
-			.notNull()
-			.references(() => users.id),
-		type: d.text({ length: 255 }).$type<AdapterAccount["type"]>().notNull(),
-		provider: d.text({ length: 255 }).notNull(),
-		providerAccountId: d.text({ length: 255 }).notNull(),
-		refresh_token: d.text(),
-		access_token: d.text(),
-		expires_at: d.integer(),
-		token_type: d.text({ length: 255 }),
-		scope: d.text({ length: 255 }),
-		id_token: d.text(),
-		session_state: d.text({ length: 255 }),
-	}),
-	(t) => [
-		primaryKey({
-			columns: [t.provider, t.providerAccountId],
-		}),
-		index("account_user_id_idx").on(t.userId),
-	],
+  "account",
+  (d) => ({
+    userId: d
+      .varchar({ length: 255 })
+      .notNull()
+      .references(() => users.id),
+    type: d.varchar({ length: 255 }).$type<AdapterAccount["type"]>().notNull(),
+    provider: d.varchar({ length: 255 }).notNull(),
+    providerAccountId: d.varchar({ length: 255 }).notNull(),
+    refresh_token: d.varchar(),
+    access_token: d.varchar(),
+    expires_at: d.integer(),
+    token_type: d.varchar({ length: 255 }),
+    scope: d.varchar({ length: 255 }),
+    id_token: d.varchar(),
+    session_state: d.varchar({ length: 255 }),
+  }),
+  (t) => [
+    primaryKey({ columns: [t.provider, t.providerAccountId] }),
+    index("account_user_id_idx").on(t.userId),
+  ]
 );
 
-// ?
-
-// SESSIONS
-export const sessions = createTable(
-	"session",
-	(d) => ({
-		sessionToken: d.text({ length: 255 }).notNull().primaryKey(),
-		userId: d
-			.text({ length: 255 })
-			.notNull()
-			.references(() => users.id),
-		expires: d.integer({ mode: "timestamp" }).notNull(),
-	}),
-	(t) => [index("session_userId_idx").on(t.userId)],
-);
-
-
-// ?
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-	user: one(users, { fields: [sessions.userId], references: [users.id] }),
+export const accountsRelations = relations(accounts, ({ one }) => ({
+  user: one(users, { fields: [accounts.userId], references: [users.id] }),
 }));
 
+export const sessions = createTable(
+  "session",
+  (d) => ({
+    sessionToken: d.varchar({ length: 255 }).notNull().primaryKey(),
+    userId: d
+      .varchar({ length: 255 })
+      .notNull()
+      .references(() => users.id),
+    expires: d.timestamp({ mode: "date", withTimezone: true }).notNull(),
+  }),
+  (t) => [index("t_user_id_idx").on(t.userId)]
+);
 
-// JWST TOKENS?
-export const t = createTable(
-	"verification_token",
-	(d) => ({
-		identifier: d.text({ length: 255 }).notNull(),
-		token: d.text({ length: 255 }).notNull(),
-		expires: d.integer({ mode: "timestamp" }).notNull(),
-	}),
-	(t) => [primaryKey({ columns: [t.identifier, t.token] })],
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  user: one(users, { fields: [sessions.userId], references: [users.id] }),
+}));
+
+export const verificationTokens = createTable(
+  "verification_token",
+  (d) => ({
+    identifier: d.varchar({ length: 255 }).notNull(),
+    token: d.varchar({ length: 255 }).notNull(),
+    expires: d.timestamp({ mode: "date", withTimezone: true }).notNull(),
+  }),
+  (t) => [primaryKey({ columns: [t.identifier, t.token] })]
 );
