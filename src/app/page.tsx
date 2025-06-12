1871eace86a7d46137dde4ff1e06fc58e8fcff7e
Ty Sanders <tysanders@tamu.edu>
2025-06-11 20:10:35 -0500

    further cleanups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
 import { Hero } from "~/app/_components/hero";
 import { Footer } from "~/app/_components/footer";
 import { auth } from "~/server/auth";
 import { HydrateClient, api } from "~/trpc/server";
 
 export default async function Home() {
-	// const hello = await api.post.hello({ text: "from tRPC" });
 	const session = await auth();
-
-	// if (session?.user) {
-	// 	void api.post.getLatest.prefetch();
-	// }
 
 	return (
 		<HydrateClient>
